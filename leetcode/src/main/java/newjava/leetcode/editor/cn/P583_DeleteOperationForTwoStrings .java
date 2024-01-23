a1163e26c9a2143fba50b97ff9af753ad4262de9
Liu zhijian <1965817202@qq.com>
2024-01-23 18:07:06 +0800

    1/23

---
+++ leetcode/src/main/java/newjava/leetcode/editor/cn/P583_DeleteOperationForTwoStrings .java
@@ -0,0 +1,70 @@
+//给定两个单词 word1 和
+// word2 ，返回使得
+// word1 和 
+// word2 相同所需的最小步数。 
+//
+// 每步 可以删除任意一个字符串中的一个字符。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入: word1 = "sea", word2 = "eat"
+//输出: 2
+//解释: 第一步将 "sea" 变为 "ea" ，第二步将 "eat "变为 "ea"
+// 
+//
+// 示例 2: 
+//
+// 
+//输入：word1 = "leetcode", word2 = "etco"
+//输出：4
+// 
+//
+// 
+//
+// 提示： 
+// 
+//
+// 
+// 1 <= word1.length, word2.length <= 500 
+// word1 和 word2 只包含小写英文字母 
+// 
+//
+// Related Topics 字符串 动态规划 👍 656 👎 0
+
+
+package leetcode.editor.cn;
+
+/**
+ * 两个字符串的删除操作
+ * @author DY
+ * @date 2024-01-23 17:36:43
+ */
+class P583_DeleteOperationForTwoStrings{
+	 public static void main(String[] args) {
+	 	 //测试代码
+	 	 Solution solution = new P583_DeleteOperationForTwoStrings().new Solution();
+	 }
+	 
+//力扣代码
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int minDistance(String word1, String word2) {
+		int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+		for (int i = 1; i <= word1.length(); i++) {
+			for (int j = 1; j <= word2.length(); j++) {
+				if(word1.charAt(i-1) == word2.charAt(j-1)){
+					dp[i][j] = dp[i-1][j-1] + 1;
+				}else{
+					dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+				}
+			}
+		}
+		return word1.length() + word2.length() - dp[word1.length()][word2.length()]*2;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
